450af4075694435ed76653eae4d8aa23560b4629
Brian Bollen <briancbollen@gmail.com>
2025-01-02 10:59:55 -0700

    Co-authored-by: Jack Wilburn <JackWilb@users.noreply.github.com>

--- new_test.py
+++ new_test.py
@@ -1,20 +1,6 @@
-import models
 import new_revisit as rvt
-import json
+
 if __name__ == "__main__":
-    comp = models.QuestionnaireComponent(
-        type='questionnaire',
-        response=[
-            models.Response(
-                id='hello',
-                type='matrix-radio',
-                required=False,
-                prompt='Test Prompt',
-                answerOptions='satisfaction5',
-                questionOptions=['Test1', 'Test2']
-            )
-        ]
-    )
     newResponse = rvt.response(
         id='hello',
         type='matrix-radio',
@@ -23,30 +9,78 @@ if __name__ == "__main__":
         answerOptions='satisfaction5',
         questionOptions=['Test1', 'Test2']
     )
-    print(newResponse)
+
+    # a = rvt.response(type='shortText')
+
+    b = rvt.response(hidden=None, id='', location=None, options=[], paramCapture=None, placeholder=None, prompt='', required=False, requiredLabel=None, requiredValue=None, secondaryText=None, type='dropdown')
+
+    c = rvt.response(id='', options=[], prompt='', required=False, type='dropdown')
+
     newResponse.data(id='test5')
-    comp_one = rvt.component(
+
+    base_comp = rvt.component(
         type='questionnaire',
         response=[],
         component_name__='Base_Test'
     )
-    print(comp_one)
-    comp = rvt.component(
-        base__=comp_one,
-        component_name__='Test'
+    
+
+    # print(comp_one)
+    comp_one = rvt.component(
+        base__=base_comp,
+        component_name__='Test',
     ).responses([
         newResponse
     ])
 
-    print(comp)
-    # print(comp.get_response(id='hello'))
+    comp_two = rvt.component(
+        base__=base_comp,
+        component_name__='TestTwo',
+    ).responses([
+        newResponse
+    ])
 
-    # newResponse = rvt.Response(
-    #     id='hello',
-    #     type='matrix-radio',
-    #     required=False,
-    #     prompt='Test Prompt',
-    #     answerOptions='satisfaction5',
-    #     questionOptions=['Test1', 'Test2']
-    # )
-    # print(json.dumps(json.loads(newResponse.model_dump_json()), indent=4))
+    # Works
+    print(comp_one.get_response('test5'))
+    # Works
+    print(comp_one.edit_response('test5', required=True))
+    # Works -- throws error when trying to change type
+    # print(comp_one.edit_response('test5', type='shortText'))
+    # Works -- throws error because options is not in matrixResponse
+    # print(comp_one.edit_response('test5', options=['12', '13']))
+    # print(comp_one.response_context(required=True))
+    comps = [comp_one, comp_two]
+    print(comp_one)
+    print(comp_two)
+    print(comp_two.component_name__)
+
+    metadata = rvt.studyMetadata(
+        authors=[],
+        date='',
+        title='',
+        version='',
+        description='',
+        organizations=[]
+    )
+
+    uiConfig = rvt.uiConfig(
+        contactEmail='test',
+        logoPath='.../fdfd',
+        sidebar=True,
+        withProgressBar=False
+    )
+
+    sequence = rvt.sequence(
+        components=[],
+        order='fixed'
+    )
+
+    studyConfig = rvt.studyConfig(
+        schema="testschema",
+        uiConfig=uiConfig,
+        studyMetadata=metadata,
+        sequence=sequence,
+        components=comps,
+        test='hello'
+    )
+    # print(studyConfig)
